9d393ddc144b9b393a2bfef5f2def163aa9fb408
PopoBoss <wlad_diaz@yahoo.com>
2022-01-17 10:47:55 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  { myName: 'Vulpix', height: 0.6, type: 'fire' },
+
+  {myName: 'Machop', height: 0.8, type: 'fighting' },
+
+  {myName: 'Growlithe', height: 0.7, type: 'fire' }
+
+];
