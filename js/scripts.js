ba666ec630fe61009752337871035bb3e88b9056
PopoBoss <wlad_diaz@yahoo.com>
2022-03-01 17:13:12 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
 let pokemonList = [
-  { myName: 'Vulpix', height: 0.6, type: 'fire' },
-
-  {myName: 'Machop', height: 0.8, type: 'fighting' },
-
-  {myName: 'Growlithe', height: 0.7, type: 'fire' },
-
+{
+  name: 'Vulpix',
+  height: 0.3,
+  type: 'fire'
+},
+{
+  name: 'Machop',
+  height: 2,
+  type: 'fighting'
+},
+{
+  name: 'Growlithe',
+  height: 0.6,
+  type: 'fire'}
 ];
+
+
+for (let i=0; i < pokemonList.length; i++){
+  if(pokemonList[i].height > 1)
+// Adding the comment "Wow, that is a big pokemon!!" to pokemon that are taller than 2m
+  {
+    document.write(pokemonList[i].name +" (Height : " + pokemonList[i].height + ")" + "- Wow this is a big pokemon!!!<br>")
+  }
+// Everything else would just show name of Pokemon and height.
+  else {
+  document.write(pokemonList[i].name + " (Height : " + pokemonList[i].height + ")<br>")
+}
+}
